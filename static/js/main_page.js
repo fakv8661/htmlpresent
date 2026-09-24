0a7707d0160ab697f01eb89e2fd93adb269dffa0
space-cat-net <mcoonsk@gmail.com>
2026-09-24 22:24:29 +0500

    Добавлен поиск

--- static/js/main_page.js
+++ static/js/main_page.js
@@ -6,28 +6,43 @@ async function updatePresentions(search = "") {
     const TABLE = document.getElementById("presentations-table");
     const STATS = document.getElementById("found-result");
     const json = await response.json();
+
     STATS.innerText = json.length;
+    TABLE.innerHTML = "";
     json.forEach((value, index) => {
       const row = `
       <tr>
-        <th scope="row">
-          <span class="format-icon">
-            HTML
-          </span>
-          ${value.title}
-        </th>
-        <td>${value.authors}</td>
-        <!-- <td>-</td> -->
-        <!-- <td>-</td> -->
+      <th scope="row">
+      <span class="format-icon">
+      HTML
+      </span>
+      ${value.title}
+      </th>
+      <td>${value.authors}</td>
+      <!-- <td>-</td> -->
+      <!-- <td>-</td> -->
       </tr>
       `;
 
-      TABLE.innerHTML += row;
+      if (
+        search === "" ||
+        value.title.toLowerCase().includes(search.toLowerCase()) ||
+        value.authors.toLowerCase().includes(search.toLowerCase()) ||
+        value.description.toLowerCase().includes(search.toLowerCase())
+      ) {
+        TABLE.innerHTML += row;
+      }
     });
   }
 }
 
 updatePresentions();
+
+document.getElementById("searchbar").oninput = (e) => {
+  console.log("s");
+  const SEARCH = document.getElementById("searchbar").value;
+  updatePresentions(SEARCH);
+};
 
 // $("#achievements-btn").click(function (e) {
 //   e.preventDefault();
