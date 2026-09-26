84001ebe055a58ed5000239027df17aa14ce90c3
space-cat-net <mcoonsk@gmail.com>
2026-09-26 20:21:17 +0500

    Реализована страница входа в админку с формой и переключателем пароля

--- static/js/admin/admin_login.js
+++ static/js/admin/admin_login.js
@@ -1,1 +1,14 @@
 // Скрипт страницы `admin_login.html`
+
+function togglePasswordVisibility() {
+  const input = document.getElementById("password-input");
+  const button = document.getElementById("password-toggle");
+
+  button.classList.toggle("concealed");
+  const isConcealed = button.classList.contains("concealed");
+  if (isConcealed) {
+    input.setAttribute("type", "password");
+  } else {
+    input.setAttribute("type", "text");
+  }
+}
